3e06c8100591ddcfbfa366ccc9946ea732ee2021
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-02-02 21:09:49 +0530

    first comit

---
+++ leetcode547.java
@@ -0,0 +1,33 @@
+package Leetcode_solution;
+
+class Solution {
+    public int findCircleNum(int[][] adj) {
+        int n = adj.length;
+        boolean[] visited = new boolean[n];
+        int count = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                bfs(adj, visited, i);
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private void bfs(int[][] adj, boolean[] visited, int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int j = 0; j < adj.length; j++) {
+                if (adj[node][j] == 1 && !visited[j]) {
+                    visited[j] = true;
+                    q.add(j);
+                }
+            }
+        }
+    }
+}
