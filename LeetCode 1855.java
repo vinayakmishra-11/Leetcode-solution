79ec4abfce4ccec9799d8aa1861995601342cdcc
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-04-19 21:47:40 +0530

    Leetcode 1855

---
+++ LeetCode 1855.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int maxDistance(int[] nums1, int[] nums2) {
+        int i = 0;
+        int j = 0;
+        int max = 0;
+
+        while (i < nums1.length && j < nums2.length) {
+
+            if (nums1[i] <= nums2[j]) {
+                max = Math.max(max, j - i);
+                j++;
+            } else {
+                i++;
+            }
+
+            // j should never be behind i
+            if (j < i) {
+                j = i;
+            }
+        }
+
+        return max;
+    }
+}
