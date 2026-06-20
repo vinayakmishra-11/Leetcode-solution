1dac35a2b02387c72283647b2ff295911d7765a0
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-20 13:03:32 +0530

    Leet code 58

---
+++ Leetcode58.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int count =0;
+        boolean flag = true;
+        for(int i =s.length()-1 ;i>=0;i--){
+
+            char ch = s.charAt(i);
+            if(count!=0 && flag==false && ch==' ') break;
+            if(ch==' '){
+                continue;
+            }
+                count++;
+                flag = false;
+         
+        }
+        return count;
+    }
+}
