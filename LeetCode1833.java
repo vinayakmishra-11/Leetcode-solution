12e73059ea1d9042fbfcea59a091f90e9cd394c6
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-06-21 09:30:20 +0530

    Leet Code 1833

---
+++ LeetCode1833.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count =0 ;
+        for(int i =0 ;i<costs.length;i++){
+            if(costs[i]<=coins){
+                count++;
+                coins=coins - costs[i];
+            }
+            else continue;
+        }
+        return  count;
+    }
+}
