bcb71e129ff0b5c8dd9a3063c32784cf1c533f35
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-07-18 19:46:22 +0530

    Time complexity -> O(nlogn) to O(n)

--- leetcode1979.java
+++ leetcode1979.java
@@ -6,7 +6,14 @@ class Solution {
     return gcd(b, a % b);
 }
     public int findGCD(int[] nums) {
-        Arrays.sort(nums);
-        return gcd(nums[0],nums[nums.length-1]);
+      int min  =Integer.MAX_VALUE;
+      int max = Integer.MIN_VALUE;
+
+      for(int i=0;i<nums.length;i++){
+        min=Math.min(nums[i],min);
+        max=Math.max(nums[i],max);
+      }
+
+      return gcd(min,max);
     }
 }
