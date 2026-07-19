b5b10ebfd2823807ebdd6334cac9bb048f2a3ef0
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-07-19 11:02:10 +0530

    Leet Code 316

---
+++ leetcode316.java
@@ -0,0 +1,37 @@
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        int[] freq = new int[26];
+
+        for(int i =0; i<s.length();i++){
+            char ch = s.charAt(i);
+            int x = ch - 'a';
+            freq[x]++;
+        }
+
+        boolean[] inStack = new boolean[26];
+        Stack<Character> stack = new Stack<>();
+
+        for(char ch : s.toCharArray()){
+            int x = ch-'a';
+            freq[x]--;
+            if (inStack[ch - 'a']) {
+                   continue;
+              }
+            while(!stack.isEmpty() && stack.peek() > ch && freq[stack.peek()-'a']>0){
+                char chh = stack.pop();
+                inStack[chh-'a'] = false;
+            }
+
+            stack.push(ch);
+            inStack[ch-'a'] = true;
+        }
+        StringBuilder ans = new StringBuilder("");
+
+        while(!stack.isEmpty()){
+            char ch = stack.pop();
+            ans.append(ch);
+        }
+
+        return ans.reverse().toString();
+    }
+}
