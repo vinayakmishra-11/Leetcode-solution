fd616635049ea20fd10979a57ad430c1ae433667
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-07-21 09:55:11 +0530

    Leet Code 119

---
+++ leetcode119.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> getRow(int n) {
+        n=n+1;
+         List<List<Integer>> ans = new ArrayList<>();
+        for(int i =0 ;i<n;i++){
+            List<Integer> l=new ArrayList<>();
+            for(int j =0;j<=i;j++){
+               if(j==0 || j==i){
+                l.add(1);
+               }
+               else{
+                l.add(ans.get(i-1).get(j)+ans.get(i-1).get(j-1));
+               }
+            }
+            ans.add(l);
+        }
+        return ans.get(n-1);
+    }
+}
