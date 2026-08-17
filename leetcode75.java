0a068e1fab17b5f99505c390c77effafff20202f
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-08-17 15:57:25 +0530

    Leet Code 75

--- leetcode75.java
+++ leetcode75.java
@@ -13,3 +13,39 @@ class Solution {
         }
     }
 }
+---------------------------------------------------------------------------
+
+// Dutch Flag alog
+
+class Solution {
+    public void sortColors(int[] nums) {
+
+        int low = 0;
+        int mid = 0;
+        int high = nums.length - 1;
+
+        while (mid <= high) {
+
+            if (nums[mid] == 0) {
+                swap(nums, low, mid);
+                low++;
+                mid++;
+            }
+
+            else if (nums[mid] == 1) {
+                mid++;
+            }
+
+            else {
+                swap(nums, mid, high);
+                high--;
+            }
+        }
+    }
+
+    public void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
