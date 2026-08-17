1c57d58d4793e2d17e803f2678c8dfeb061147f9
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-08-17 12:14:54 +0530

    Leet Code 184

---
+++ leetcode184.sql
@@ -0,0 +1,13 @@
+
+SELECT
+    d.name AS Department,
+    e.name AS Employee,
+    e.salary AS Salary
+FROM Employee e
+JOIN Department d
+    ON e.departmentId = d.id
+WHERE e.salary = (
+    SELECT MAX(e2.salary)
+    FROM Employee e2
+    WHERE e2.departmentId = e.departmentId
+);
