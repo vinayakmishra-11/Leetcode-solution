2cfd40bf21ed17cbd77de57de6a7ee478cfc9c03
vinayak mishra <vinayakmishra11052004@gmail.com>
2026-08-25 17:59:58 +0530

    Leet Code 3718 add new version

--- leetcode3718.java
+++ leetcode3718.java
@@ -14,4 +14,20 @@ class Solution {
 
     }
 }
+-----------------------------------------------------------------------------------------------
+class Solution {
+    public int missingMultiple(int[] nums, int k) {
+     HashSet<Integer> set = new HashSet<>();
 
+     for(int i =0 ;i<nums.length;i++){
+        set.add(nums[i]);
+     } 
+     for(int i =1 ;i<=nums.length+1;i++){
+        int x = k*i;
+        if(set.contains(x)) continue;
+        return x;
+     }
+     return 0;  
+
+    }
+}
